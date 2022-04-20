4ecaac503dcc1333d0544787ba365aacea0c379c
Alexander Hübener <alexander.huebener@it-designers.de>
2022-04-20 10:56:06 +0200

    fixed doc

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,22 @@
 
 //! # Driver for MICS-VZ-89TE sensor
 //!
-//! This driver can be used to read co2 and voc measurements of the MICS-VZ-89TE sensor. Co2 is measured in ppm in the range from 400 to 2000.
-//! Voc is measured in ppb in the range from 0 to 1000. At startup the sensor needs around 15 minutes to deliver a co2 value, noted in the datasheet.
+//! This driver can be used to read CO2 and voc measurements of the MICS-VZ-89TE sensor. CO2 is measured in ppm in range from 400 to 2000.
+//! VOC is measured in ppb in range from 0 to 1000. At startup the sensor needs around 15 minutes to deliver a valid CO2 value, as noted in the datasheet.
 //!
-//! To use this driver, the i2c bus has to be set to a max baudrate of 100_000.
+//! To use this driver, the I2C bus has to be set to a max baudrate of 100_000.
 //!
 //! # Example Usage
 //! ```
-//! let driver = MicsVz89Te::new(i2c);
+//! let mut delay = ...; // delay struct from board
+//! let i2c = ...; // I2C bus to use
 //!
+//! let mut device = MicsVz89Te::new(i2c);
+//! let measurements = device.read_status(&mut delay).unwrap();
+//!
+//! let co2 = measurements.co2;
+//! let voc = measurements.voc;
 //! ```
-//!
 
 use embedded_hal::blocking::{
     delay::DelayMs,
@@ -25,13 +30,14 @@ static MICS_VZ_89TE_ADDR_CMD_GETSTATUS: u8 = 0x0C; // This command is used to re
 #[allow(dead_code)]
 static MICS_VZ_89TE_DATE_CODE: u8 = 0x0D;
 
+/// Returned measurements by the sensor
 #[derive(Debug, Clone, Copy)]
 pub struct Measurements {
     pub co2: f32,
     pub voc: f32,
 }
 
-/// Baudrate must be set to max 100_000 Hz
+/// Driver for MICS-VZ-89TE sensor
 pub struct MicsVz89Te<I2C> {
     i2c: I2C,
     buffer: [u8; 7],
@@ -41,6 +47,7 @@ impl<I2C, E> MicsVz89Te<I2C>
 where
     I2C: Read<Error = E> + Write<Error = E>,
 {
+    /// Create new driver on the supplied i2c bus.
     pub fn new(i2c: I2C) -> Self {
         Self {
             i2c,
@@ -48,7 +55,8 @@ where
         }
     }
 
-    pub fn read_status(&mut self, delay: &mut impl DelayMs<u16>) -> Result<Measurements, E> {
+    /// Read measurements from sensor.
+    pub fn read_measurements(&mut self, delay: &mut impl DelayMs<u16>) -> Result<Measurements, E> {
         self.i2c.write(
             MICS_VZ_89TE_ADDR,
             &[MICS_VZ_89TE_ADDR_CMD_GETSTATUS, 0, 0, 0, 0, 0xF3],
