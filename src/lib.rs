05e5c93f4684944729e65877449cf1149bd8ac9b
Alexander Hübener <alexander.huebener@it-designers.de>
2022-04-20 11:36:07 +0200

    added test for read function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![no_std]
+#![cfg_attr(not(test), no_std)]
 
 //! # Driver for MICS-VZ-89TE sensor
 //!
@@ -8,7 +8,7 @@
 //! To use this driver, the I2C bus has to be set to a max baudrate of 100_000.
 //!
 //! # Example Usage
-//! ```
+//! ```ignore
 //! let mut delay = ...; // delay struct from board
 //! let i2c = ...; // I2C bus to use
 //!
@@ -64,6 +64,8 @@ where
         delay.delay_ms(100);
         self.i2c.read(MICS_VZ_89TE_ADDR, &mut self.buffer)?;
 
+        // TODO check checksum
+
         let co2 = (self.buffer[1] - 13) as f32 * (1600.0 / 229.0) + 400.0; // ppm: 400 .. 2000
         let voc = (self.buffer[0] - 13) as f32 * (1000.0 / 229.0); // ppb: 0 .. 1000
 
@@ -75,4 +77,31 @@ where
 fn gen_checksum(byte_array: &[u8]) -> u8 {
     let sum = byte_array.iter().fold(0u16, |a, v| a + (*v as u16));
     0xFF - (sum as u8 + (sum / 0x0100) as u8)
+}
+
+#[cfg(test)]
+mod test {
+    use super::MicsVz89Te;
+    use core::assert_eq;
+    use embedded_hal_mock::{
+        delay::MockNoop as DelayMock,
+        i2c::{Mock as I2cMock, Transaction as I2cTransaction},
+    };
+    use std::vec;
+
+    #[test]
+    fn test_read_measurements() {
+        let expectations = [
+            I2cTransaction::write(0x70, vec![0x0C, 0, 0, 0, 0, 0xF3]),
+            I2cTransaction::read(0x70, vec![0x27, 0x3C, 0, 0xBA, 0xBA, 0, 0x27]),
+        ];
+        let i2c = I2cMock::new(&expectations);
+        let mut delay = DelayMock::new();
+
+        let mut device = MicsVz89Te::new(i2c);
+        let measurements = device.read_measurements(&mut delay).unwrap();
+
+        assert_eq!(measurements.co2 as u32, 728);
+        assert_eq!(measurements.voc as u32, 113);
+    }
 }
